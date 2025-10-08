096c081cf65130f5268bdf4beb801581810d3e40
dynst <148708712+dynst@users.noreply.github.com>
2025-10-08 00:00:00 +0000

    s/xit/it.skip/

--- packages/faucet-client/src/faucetclient.spec.ts
+++ packages/faucet-client/src/faucetclient.spec.ts
@@ -21,22 +21,22 @@ describe("FaucetClient", () => {
     expect(new FaucetClient("https://localhost/")).toBeTruthy();
   });
 
-  (enabled ? it : xit)("can be used to credit a wallet", async () => {
+  (enabled ? it : it.skip)("can be used to credit a wallet", async () => {
     const faucet = new FaucetClient(faucetUrl);
     await faucet.credit(defaultAddress, primaryToken);
   });
 
-  (enabled ? it : xit)("can be used to credit a wallet with a different token", async () => {
+  (enabled ? it : it.skip)("can be used to credit a wallet with a different token", async () => {
     const faucet = new FaucetClient(faucetUrl);
     await faucet.credit(defaultAddress, secondaryToken);
   });
 
-  (enabled ? it : xit)("throws for invalid ticker", async () => {
+  (enabled ? it : it.skip)("throws for invalid ticker", async () => {
     const faucet = new FaucetClient(faucetUrl);
     await expectAsync(faucet.credit(defaultAddress, "ETH")).toBeRejectedWithError(/token is not available/i);
   });
 
-  (enabled ? it : xit)("throws for invalid address", async () => {
+  (enabled ? it : it.skip)("throws for invalid address", async () => {
     const faucet = new FaucetClient(faucetUrl);
 
     for (const address of ["be5cc2cc05db2cdb4313c18306a5157291cfdcd1", "1234L"]) {
